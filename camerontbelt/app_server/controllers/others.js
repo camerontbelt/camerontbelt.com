bafd6c1f2abbd26dfbb4c9223523bb110ce87c1a
Cameron Belt <camerontbelt@gmail.com>
2017-12-27 11:13:43 -0600

    Removed duplicate folder.

--- camerontbelt/app_server/controllers/others.js
+++
@@ -1,8 +0,0 @@
-/*GET home page*/
-module.exports.about = function(req,res)
-{
-    res.render('generic-text', { 
-        title: 'About Loc8r',
-        content: 'Loc8r was created to help people find places to sid down and get a bit of work done. \n\nLorem ipusm'
-    });
-};
